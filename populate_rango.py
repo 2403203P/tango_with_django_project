20e750e554fe4cedce7de460523a25402f31a7ab
Jose Luis Povedano Poyato <2403203P@student.gla.ac.uk>
2020-01-26 19:08:10 +0000

    Chapter 6 DONE

--- populate_rango.py
+++ populate_rango.py
@@ -12,13 +12,15 @@ def populate():
 # This might seem a little bit confusing, but it allows us to iterate 
 # through each data structure, and add the data to our models.
     python_pages=[{'title': 'Official Python Tutorial', 
-        'url':'http://docs.python.org/3/tutorial/'}, 
+        'url':'http://docs.python.org/3/tutorial/', 'views':97}, 
         {'title':'How to Think like a Computer Scientist', 
-        'url':'http://www.greenteapress.com/thinkpython/'}, 
+        'url':'http://www.greenteapress.com/thinkpython/', 'views':65}, 
         {'title':'Learn Python in 10 Minutes', 
-        'url':'http://www.korokithakis.net/tutorials/python/'}]
-    django_pages=[ {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},  {'title':'Django Rocks', 'url':'http://www.djangorocks.com/'}, {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/'}]
-    other_pages = [ {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/'}, {'title':'Flask', 'url':'http://flask.pocoo.org'} ] 
+        'url':'http://www.korokithakis.net/tutorials/python/', 'views':88}]
+    django_pages=[ {'title':'Official Django Tutorial', 'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views':25}, 
+        {'title':'Django Rocks', 'url':'http://www.djangorocks.com/', 'views':48}, 
+        {'title':'How to Tango with Django', 'url':'http://www.tangowithdjango.com/', 'views':41}]
+    other_pages = [ {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/', 'views': 36}, {'title':'Flask', 'url':'http://flask.pocoo.org', 'views':17} ] 
     
     cats = {'Python': {'pages': python_pages, 'views':128, 'likes':64}, 'Django': {'pages': django_pages, 'views':64, 'likes':32}, 'Other Frameworks': {'pages': other_pages, 'views':32, 'likes':16} }
      # If you want to add more categories or pages add them to the dictionaries above
@@ -27,7 +29,7 @@ def populate():
     for cat, cat_data in cats.items(): #The categories name acts as the key for the outside dictionary, meanwhile the categories data is in the nested dictionary
         c=add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
             
     #Print out the categories we have added
     for c in Category.objects.all():
