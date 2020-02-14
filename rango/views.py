2dfbed5127733e7148e5e7cda072fb3a6f171062
Jose Luis Povedano Poyato <2403203P@student.gla.ac.uk>
2020-02-14 16:44:03 +0000

    Chapter 10 Fully Fixed

--- rango/views.py
+++ rango/views.py
@@ -171,8 +171,8 @@ def get_server_side_cookie(request, cookie, default_val=None):
 
 def visitor_cookie_handler(request): # Get the number of visits to the site. We use the COOKIES.get() function to obtain the visits cookie. If the cookie exists, the value returned is casted to an integer. 
 # If the cookie doesn't exist, then the default value of 1 is used. 
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now())) 
+    visits = int(get_server_side_cookie(request,'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now())) 
     last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
     # If it's been more than a day since the last visit... 
     if (datetime.now() - last_visit_time).days > 0: 
